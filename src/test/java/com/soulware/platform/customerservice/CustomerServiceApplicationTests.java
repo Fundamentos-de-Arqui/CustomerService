f1ed345f1a68a9dc1166a5835fdbdd55ba12d112
jsyumg <u202210838@upc.edu.pe>
2025-09-30 23:05:23 -0500

    first commit

---
+++ src/test/java/com/soulware/platform/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulware.platform.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
